78a9a70b7fb93029ca2414232d23e7230faa3850
JunkyByte <adriano.donninelli@hotmail.it>
2018-08-26 11:43:08 +0200

    FIX commit add ignore

--- autopy.js
+++ autopy.js
@@ -1,105 +1,97 @@
-/*    var gist_button = function () {
-        if (!IPython.toolbar) {
-            $([IPython.events]).on("app_initialized.NotebookApp", gist_button);
-            return;
-        }
-
-        if ($("#gist_notebook").length === 0) {
-            IPython.toolbar.add_buttons_group([
-                {
-                    'label'   : 'gist',
-                    'help'    : 'Share notebook as gist',
-                    'icon'    : 'fa-share',
-                    'callback': gist_notebook,
-                    'id'      : 'gist_notebook'
-                },
-            ]);
-        }
-    }
-*/
-
 var tag_name = 'autopy';
+var highLightedColor = 'rgba(21, 233, 233, 0.2)';
 
-var push_on_not_tagged = function(cells){
+var set_bg = function(cell){
+  state = cell.metadata.autopy;
+  if (state == null){
+    add_metadata(cell);
+    set_bg(cell);
+  }
+
+  if (state){
+    cell.input[0].childNodes[1].style.backgroundColor = highLightedColor;
+  } else
+    cell.input[0].childNodes[1].style.backgroundColor = '';
+}
+
+var add_metadata = function(cell){
+  cell.metadata.autopy = false;
+}
+
+var change_state = function(cell){
+  state = cell.metadata.autopy;
+  if (state == null){
+    add_metadata(cell);
+    change_state(cell);
+  }
+
+  if (! state){
+    cell.metadata.autopy = true;
+    set_bg(cell);
+  } else
+    cell.metadata.autopy = false;
+    set_bg(cell);
+}
+
+var clear_all_states = function(){
+  cells = Jupyter.notebook.get_cells();
   for (var i=0; i < cells.length; i++){
-    tags = cells[i]['_metadata']['tags'];
-    if (! tags)
+    state = cells[i].metadata.autopy;
+    if (state == null){
+      add_metadata();
       continue;
-    if (! tags.contain(tag_name))
-      cells[i]['_metadata']['tags'].push(tag_name);
+    }
+
+    cells[i].metadata.autopy = false;
   }
 }
 
-var clear_tags = function(cells){
+var restoreBackgrounds = function(){
+  var cells = Jupyter.notebook.get_cells();
   for (var i=0; i < cells.length; i++){
-    tags = cells[i]['_metadata']['tags'];
-    if (! tags)
-      continue;
-    index = tags.indexOf(tag_name);
-    if (index > -1)
-      cells[i]['_metadata']['tags'].splice(index, 1);
+    set_bg(cells[i]);
   }
 }
 
-var clear_all_tags = function(cells){
-  for (var i=0; i < cells.length; i++){
-    tags = cells[i]['_metadata']['tags'];
-    if (! tags)
-      continue;
+var mark_selected = function(){
+  var selected_cell = Jupyter.notebook.get_selected_cell();
+  change_state(selected_cell);
+}
 
-    cells[i]['_metadata']['tags'] = [];
+var copy_cells = function(){
+  console.log('pressed');
+}
+
+var copy_button = function () {
+  if (!IPython.toolbar) {
+    $([IPython.events]).on("app_initialized.NotebookApp", copy_button);
+    return;
+  }
+
+  if ($("#gist_notebook").length === 0) {
+      IPython.toolbar.add_buttons_group([
+        {
+        'label'   : 'autopy',
+        'help'    : 'Copy to py file selected cells',
+        'icon'    : 'fa-arrow-circle-o-right',
+        'callback': copy_cells,
+        'id'      : 'copy_button'
+        },
+        ]);
   }
 }
 
 define([
-    'base/js/namespace'
+  'base/js/namespace'
 ], function(Jupyter) {
-    var exports = {};
+  var exports = {};
 
+  // Wait for notification that the app is ready
+  exports.load_ipython_extension = function() {
+    restoreBackgrounds();
+    copy_button();
+    Jupyter.keyboard_manager.command_shortcuts.add_shortcut('t', mark_selected);
+  };
 
-    // Show counts of cell types
-    var show_stats = function() {
-
-        // Get counts of each cell type
-        var cells = Jupyter.notebook.get_cells();
-        var selected_cells = Jupyter.notebook.get_selected_cell();
-        clear_all_tags(cells);
-        console.log(selected_cells);
-        console.log(cells[0]['_metadata']['tags']);
-        cells[0]['_metadata']['tags'].push('test2');
-        console.log(cells[0]['_metadata']['tags']);
-
-        var hist = {};
-        for(var i=0; i < cells.length; i++) {
-            var ct = cells[i].cell_type;
-            if(hist[ct] === undefined) {
-                hist[ct] = 1;
-            } else {
-                hist[ct] += 1;
-            }
-        }
-
-        // Build paragraphs of cell type and count
-        var body = $('<div>');
-        for(var ct in hist) {
-            $('<p>').text(ct+': '+hist[ct]).appendTo(body);
-        }
-
-        // Show a modal dialog with the stats
-        Jupyter.dialog.modal({
-            title: "Notebook Stats",
-            body: body,
-            buttons : {
-                "OK": {}
-            }
-        });
-    };
-
-    // Wait for notification that the app is ready
-    exports.load_ipython_extension = function() {
-        // Then register command mode hotkey "s" to show the dialog
-        Jupyter.keyboard_manager.command_shortcuts.add_shortcut('s', show_stats);
-    };
-
-    return exports;
+  return exports;
 });
