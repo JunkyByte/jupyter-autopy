5ac8facc982bee8dfb3a1e33f8fac655c51ca3dc
JunkyByte <adriano.donninelli@hotmail.it>
2018-08-27 02:24:26 +0200

    Update, working yaml / nb extension format

--- autopy.js
+++ autopy.js
@@ -1,97 +1,117 @@
-var tag_name = 'autopy';
-var highLightedColor = 'rgba(21, 233, 233, 0.2)';
+define([
+  'require',
+  'jquery',
+  'base/js/namespace'
+], function(
+  requirejs,
+  $,
+  Jupyter) {
 
-var set_bg = function(cell){
-  state = cell.metadata.autopy;
-  if (state == null){
-    add_metadata(cell);
-    set_bg(cell);
-  }
+    var tag_name = 'autopy';
 
-  if (state){
-    cell.input[0].childNodes[1].style.backgroundColor = highLightedColor;
-  } else
-    cell.input[0].childNodes[1].style.backgroundColor = '';
-}
+    var set_bg = function(cell){
+      state = cell.metadata.autopy;
+      if (state == null){
+        add_metadata(cell);
+        set_bg(cell);
+      }
 
-var add_metadata = function(cell){
-  cell.metadata.autopy = false;
-}
-
-var change_state = function(cell){
-  state = cell.metadata.autopy;
-  if (state == null){
-    add_metadata(cell);
-    change_state(cell);
-  }
-
-  if (! state){
-    cell.metadata.autopy = true;
-    set_bg(cell);
-  } else
-    cell.metadata.autopy = false;
-    set_bg(cell);
-}
-
-var clear_all_states = function(){
-  cells = Jupyter.notebook.get_cells();
-  for (var i=0; i < cells.length; i++){
-    state = cells[i].metadata.autopy;
-    if (state == null){
-      add_metadata();
-      continue;
+      if (state){
+        cell.input[0].childNodes[1].style.backgroundColor = params.highlightedColor;
+      } else
+        cell.input[0].childNodes[1].style.backgroundColor = '';
     }
 
-    cells[i].metadata.autopy = false;
-  }
-}
+    var add_metadata = function(cell){
+      cell.metadata.autopy = false;
+    }
 
-var restoreBackgrounds = function(){
-  var cells = Jupyter.notebook.get_cells();
-  for (var i=0; i < cells.length; i++){
-    set_bg(cells[i]);
-  }
-}
+    var change_state = function(cell){
+      state = cell.metadata.autopy;
+      if (state == null){
+        add_metadata(cell);
+        change_state(cell);
+      }
 
-var mark_selected = function(){
-  var selected_cell = Jupyter.notebook.get_selected_cell();
-  change_state(selected_cell);
-}
+      if (! state){
+        cell.metadata.autopy = true;
+        set_bg(cell);
+      } else
+        cell.metadata.autopy = false;
+        set_bg(cell);
+    }
 
-var copy_cells = function(){
-  console.log('pressed');
-}
+    var clear_all_states = function(){
+      cells = Jupyter.notebook.get_cells();
+      for (var i=0; i < cells.length; i++){
+        state = cells[i].metadata.autopy;
+        if (state == null){
+          add_metadata();
+          continue;
+        }
 
-var copy_button = function () {
-  if (!IPython.toolbar) {
-    $([IPython.events]).on("app_initialized.NotebookApp", copy_button);
-    return;
-  }
+        cells[i].metadata.autopy = false;
+      }
+    }
 
-  if ($("#gist_notebook").length === 0) {
-      IPython.toolbar.add_buttons_group([
-        {
-        'label'   : 'autopy',
-        'help'    : 'Copy to py file selected cells',
-        'icon'    : 'fa-arrow-circle-o-right',
-        'callback': copy_cells,
-        'id'      : 'copy_button'
-        },
-        ]);
-  }
-}
+    var restoreBackgrounds = function(){
+      var cells = Jupyter.notebook.get_cells();
+      for (var i=0; i < cells.length; i++){
+        set_bg(cells[i]);
+      }
+    }
 
-define([
-  'base/js/namespace'
-], function(Jupyter) {
-  var exports = {};
+    var mark_selected = function(){
+      var selected_cell = Jupyter.notebook.get_selected_cell();
+      change_state(selected_cell);
+    }
 
-  // Wait for notification that the app is ready
-  exports.load_ipython_extension = function() {
-    restoreBackgrounds();
-    copy_button();
-    Jupyter.keyboard_manager.command_shortcuts.add_shortcut('t', mark_selected);
+    var copy_cells = function(){
+      console.log('pressed');
+    }
+
+    var copy_button = function () {
+      if (!IPython.toolbar) {
+        $([IPython.events]).on("app_initialized.NotebookApp", copy_button);
+        return;
+      }
+
+      if ($("#gist_notebook").length === 0) {
+          IPython.toolbar.add_buttons_group([
+            {
+            'label'   : 'autopy',
+            'help'    : 'Copy to py file selected cells',
+            'icon'    : 'fa-arrow-circle-o-right',
+            'callback': copy_cells,
+            'id'      : 'copy_button'
+            },
+            ]);
+      }
+    }
+
+  var params = {
+      highlightedColor : '',
+      highlightedColorMain : ''
   };
 
-  return exports;
+  var initialize = function () {
+    $.extend(true, params, Jupyter.notebook.config.data['autopy']);
+    if (params.highlightedColor == '')
+      params.highlightedColor = 'rgba(21, 233, 233, 0.2)';
+    if (params.highlightedColorMain == '')
+      params.highlightedColorMain = 'rgba(228, 62, 62, 0.5)';
+
+    restoreBackgrounds();
+    copy_button();
+
+    Jupyter.keyboard_manager.command_shortcuts.add_shortcut('t', mark_selected);
+  }
+
+  var load_jupyter_extension = function() {
+    return Jupyter.notebook.config.loaded.then(initialize);
+  };
+
+  return {
+    load_jupyter_extension : load_jupyter_extension,
+    load_ipython_extension : load_jupyter_extension  };
 });
